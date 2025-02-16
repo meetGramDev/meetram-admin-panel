35dd20f43bd176a2ed46441349eee58e9b40b446
VyacheslavSavchik <rothmans.830@gmail.com>
2025-02-16 19:51:19 +0300

    change: styles

--- app/[locale]/(auth)/layout.tsx
+++ app/[locale]/(auth)/layout.tsx
@@ -2,7 +2,7 @@ import { type PropsWithChildren } from 'react'
 
 export default function AuthLayout({ children }: PropsWithChildren) {
   return (
-    <main className={'flex min-h-screen items-center justify-center px-4 sm:mt-[-3rem]'}>
+    <main className={'flex min-h-[100dvh] items-center justify-center px-4 sm:mt-[-3rem]'}>
       {children}
     </main>
   )
