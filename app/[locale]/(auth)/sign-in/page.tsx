44be5e6f887cba42f2ff00d55ef4b0e693b86724
VyacheslavSavchik <rothmans.830@gmail.com>
2025-02-26 16:21:18 +0300

    fix: add suspense in sign in page

--- app/[locale]/(auth)/sign-in/page.tsx
+++ app/[locale]/(auth)/sign-in/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'
+import { Suspense, useState } from 'react'
 
 import { type SignInFields, SignInForm, useSignInMutation } from '@/src/features/auth/signIn'
 import { apolloErrorsHandler } from '@/src/shared/lib'
@@ -37,5 +37,9 @@ export default function SignInPage() {
     }
   }
 
-  return <SignInForm error={error} onSubmit={handleSubmitForm} />
+  return (
+    <Suspense>
+      <SignInForm error={error} onSubmit={handleSubmitForm} />
+    </Suspense>
+  )
 }
