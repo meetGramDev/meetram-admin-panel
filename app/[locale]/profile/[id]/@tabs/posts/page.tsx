ed9e423f4d683b1153e007aba43e2b833a8a3e64
m1rade <forx98@gmail.com>
2025-02-27 21:41:15 +0300

    chore(user-posts): add error boundary component

--- app/[locale]/profile/[id]/@tabs/posts/page.tsx
+++ app/[locale]/profile/[id]/@tabs/posts/page.tsx
@@ -7,28 +7,33 @@ import {
 } from '@/src/entities/post'
 import { UserPosts } from '@/src/pages_layer/user-profile'
 import { PreloadQuery } from '@/src/shared/api/apollo-client'
+import { ErrorBoundary } from '@/src/shared/ui'
 import { ProfileTabValues } from '@/src/widgets/tabs'
 import { Loader, TabContent } from '@meetgram/ui-kit'
 
 export default function Page({ params: { id } }: { params: { id: string } }) {
   return (
     <TabContent value={ProfileTabValues.posts}>
-      <PreloadQuery<Get_User_PostsQuery, Get_User_PostsQueryVariables>
-        query={Get_User_PostsDocument}
-        variables={{ userId: +id }}
+      <ErrorBoundary
+        fallback={<p className={'text-center text-h1 text-danger-500'}>Something went wrong</p>}
       >
-        {queryRef => (
-          <Suspense
-            fallback={
-              <div className={'flex justify-center'}>
-                <Loader />
-              </div>
-            }
-          >
-            <UserPosts queryRef={queryRef} />
-          </Suspense>
-        )}
-      </PreloadQuery>
+        <PreloadQuery<Get_User_PostsQuery, Get_User_PostsQueryVariables>
+          query={Get_User_PostsDocument}
+          variables={{ userId: +id }}
+        >
+          {queryRef => (
+            <Suspense
+              fallback={
+                <div className={'flex justify-center'}>
+                  <Loader />
+                </div>
+              }
+            >
+              <UserPosts queryRef={queryRef} />
+            </Suspense>
+          )}
+        </PreloadQuery>
+      </ErrorBoundary>
     </TabContent>
   )
 }
