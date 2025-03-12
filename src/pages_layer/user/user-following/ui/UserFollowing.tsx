37cdd99d96935b999539736da17d15eae7f82bb5
mrAxekash <aliaxandr_ku@mail.ru>
2025-03-12 23:30:00 +0300

    feat: add filter options

--- src/pages_layer/user/user-following/ui/UserFollowing.tsx
+++ src/pages_layer/user/user-following/ui/UserFollowing.tsx
@@ -1,17 +1,29 @@
 'use client'
 
-import * as Types from '@/src/shared/api/models.gen'
+import {
+  type FollowPaginationModel,
+  type QueryGetFollowingArgs,
+  SortDirection,
+} from '@/src/shared/api/models.gen'
 import { PROFILE } from '@/src/shared/routes'
 import { ProfileTabValues } from '@/src/widgets/tabs'
-import { followersListTableHeaders } from '@/src/widgets/users-list/table/const/users-list-table-headers'
 import {
+  type followersListTableHeadKeysType,
+  followersListTableHeaders,
+} from '@/src/widgets/users-list/table/const/users-list-table-headers'
+import {
+  PAGE_PARAM_KEY,
   PAGE_SIZE_PARAM_KEY,
+  SORT_BY_PARAM_KEY,
+  SORT_PARAM_KEY,
   paginationPageSize,
 } from '@/src/widgets/users-list/table/model/pagination-config'
+import { SortDirectionTable } from '@/src/widgets/users-list/table/model/table.types'
 import { gql, useQuery } from '@apollo/client'
 import * as Apollo from '@apollo/client'
 import {
   Button,
+  type ITableHead,
   Pagination,
   Table,
   TableBody,
@@ -56,41 +68,18 @@ export const Get_FollowingDocument = gql`
 
 const defaultOptions = {} as const
 
-export type Get_FollowingQueryVariables = Types.Exact<{
-  pageNumber?: Types.InputMaybe<Types.Scalars['Int']['input']>
-  pageSize?: Types.InputMaybe<Types.Scalars['Int']['input']>
-  sortBy?: Types.InputMaybe<Types.Scalars['String']['input']>
-  sortDirection?: Types.InputMaybe<Types.SortDirection>
-  userId: Types.Scalars['Int']['input']
-}>
 export type Get_FollowingQuery = {
   __typename?: 'Query'
-  getFollowing: {
-    __typename?: 'FollowPaginationModel'
-    items: Array<{
-      __typename?: 'Follow'
-      createdAt: any
-      id: number
-      userId: number
-      userName?: null | string
-    }>
-    page: number
-    pageSize: number
-    pagesCount: number
-    totalCount: number
-  }
+  getFollowing: FollowPaginationModel
 }
 
 export function useGet_FollowingQuery(
-  baseOptions: ({ skip: boolean } | { skip?: boolean; variables: Get_FollowingQueryVariables }) &
-    Apollo.QueryHookOptions<Get_FollowingQuery, Get_FollowingQueryVariables>
+  baseOptions: ({ skip: boolean } | { skip?: boolean; variables: QueryGetFollowingArgs }) &
+    Apollo.QueryHookOptions<Get_FollowingQuery, QueryGetFollowingArgs>
 ) {
   const options = { ...defaultOptions, ...baseOptions }
 
-  return Apollo.useQuery<Get_FollowingQuery, Get_FollowingQueryVariables>(
-    Get_FollowingDocument,
-    options
-  )
+  return Apollo.useQuery<Get_FollowingQuery, QueryGetFollowingArgs>(Get_FollowingDocument, options)
 }
 
 export const UserFollowing = () => {
@@ -98,30 +87,61 @@ export const UserFollowing = () => {
   const searchParams = useSearchParams()
   const pathname = usePathname()
   const router = useRouter()
-  const params = useParams()
+  const paramsId = useParams()
 
   const itemsPerPage = searchParams.get(PAGE_SIZE_PARAM_KEY) || paginationPageSize[1]
-
-  const parsedUserId = params?.id ? Number(params?.id) : null
+  const parsedUserId = paramsId?.id ? Number(paramsId?.id) : null
+  const userSortBy = searchParams.get(SORT_BY_PARAM_KEY) || followersListTableHeaders[3].key
+  const sortDirParam = searchParams.get(SORT_PARAM_KEY)
+  const sortDir = sortDirParam ? +sortDirParam : SortDirectionTable.DESC
+  const currentPage = searchParams.get(PAGE_PARAM_KEY) || 1
+  const params = new URLSearchParams(searchParams)
 
   const { data, error, loading } = useGet_FollowingQuery({
     variables: {
-      pageNumber: 1,
+      pageNumber: +currentPage,
       pageSize: +itemsPerPage,
-      sortBy: 'createdAt',
-      sortDirection: 'desc',
+      sortBy: userSortBy,
+      sortDirection: sortDir === SortDirectionTable.DESC ? SortDirection.Desc : SortDirection.Asc,
       userId: parsedUserId!,
     },
   })
+  const saveSearchParams = () => {
+    router.replace(`${pathname}?${params.toString()}`)
+  }
+  const handleOnPageChange = (page: number) => {
+    params.set(PAGE_PARAM_KEY, String(page))
+    saveSearchParams()
+  }
+  const handleItemsPerPageChange = (itemsPerPage: number) => {
+    params.set(PAGE_SIZE_PARAM_KEY, String(itemsPerPage))
+    saveSearchParams
+  }
+  const handleChangeSorting = (header: ITableHead<followersListTableHeadKeysType>) => {
+    const sortDirection =
+      // eslint-disable-next-line no-nested-ternary
+      userSortBy === header.key
+        ? sortDir === SortDirectionTable.DESC
+          ? SortDirectionTable.ASC
+          : SortDirectionTable.DESC
+        : SortDirectionTable.ASC
+
+    params.set(SORT_PARAM_KEY, String(sortDirection))
+    params.set(SORT_BY_PARAM_KEY, header.key)
+
+    saveSearchParams()
+  }
 
   return (
     <>
       <div>
-        <Table>
+        <Table className={'mb-9'}>
           <TableHeader>
             <TableRow>
               {followersListTableHeaders.map(header => (
                 <TableHead
+                  sort={userSortBy === header.key ? sortDir : undefined}
+                  onClick={() => handleChangeSorting(header)}
                   className={
                     'transition-colors hover:border-0 hover:shadow-sm hover:shadow-neutral-100/50'
                   }
@@ -137,7 +157,7 @@ export const UserFollowing = () => {
             {data?.getFollowing?.items?.map(following => {
               return (
                 <TableRow key={following.userId}>
-                  <TableCell>{following.userId}</TableCell>
+                  <TableCell className={'flex items-center gap-3'}>{following.userId}</TableCell>
                   <TableCell>{following.userName}</TableCell>
                   <TableCell>
                     {
@@ -160,17 +180,17 @@ export const UserFollowing = () => {
           </TableBody>
         </Table>
       </div>
-      {/*<div className={'w-full md:w-1/2'}>*/}
-      {/*  {data?.getFollowing.items.length && (*/}
-      {/*    <Pagination*/}
-      {/*      currentPage={data?.getFollowing.page}*/}
-      {/*      pageCount={data?.getFollowing.pagesCount}*/}
-      {/*      onPageChange={handleOnPageChange}*/}
-      {/*      options={paginationPageSize}*/}
-      {/*      onPerPageChange={handleItemsPerPageChange}*/}
-      {/*    />*/}
-      {/*  )}*/}
-      {/*</div>*/}
+      <div className={'w-full md:w-1/2'}>
+        {data?.getFollowing.items.length && (
+          <Pagination
+            currentPage={data?.getFollowing?.page}
+            pageCount={data?.getFollowing?.pagesCount}
+            onPageChange={handleOnPageChange}
+            options={paginationPageSize}
+            onPerPageChange={handleItemsPerPageChange}
+          />
+        )}
+      </div>
     </>
   )
 }
