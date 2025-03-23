4aee78afb02a022b67c7ba7b46f1b531f995a82f
VyacheslavSavchik <rothmans.830@gmail.com>
2025-03-23 17:14:35 +0300

    add: UserPayments.tsx

--- src/widgets/users-list/table/const/users-list-table-headers.ts
+++ src/widgets/users-list/table/const/users-list-table-headers.ts
@@ -1,8 +1,9 @@
-import type { Follow, User } from '@/src/shared/api'
+import type { Follow, Payment, User } from '@/src/shared/api'
 import type { ITableHead } from '@meetgram/ui-kit'
 
 export type usersListTableHeadKeysType = keyof User
 export type followersListTableHeadKeysType = keyof Follow
+export type paymentsListTableHeadKeysType = keyof Payment
 
 export const usersListTableHeaders: ITableHead<usersListTableHeadKeysType>[] = [
   { id: 1, key: 'id', label: 'User ID' },
@@ -16,4 +17,12 @@ export const followersListTableHeaders: ITableHead<followersListTableHeadKeysTyp
   { id: 2, key: 'userName', label: 'Username' },
   { id: 3, key: 'userId', label: 'Profile link' },
   { id: 4, key: 'createdAt', label: 'Subscription Date' },
+]
+
+export const paymentListTableHeaders: ITableHead<paymentsListTableHeadKeysType>[] = [
+  { id: 1, key: 'createdAt', label: 'Date of Payment' },
+  { id: 2, key: 'endDate', label: 'End date of subscription' },
+  { id: 3, key: 'amount', label: 'Amount, $' },
+  { id: 4, key: 'type', label: 'Subscription Type' },
+  { id: 5, key: 'paymentMethod', label: 'Payment Type' },
 ]
