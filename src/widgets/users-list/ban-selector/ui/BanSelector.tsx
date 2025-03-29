3d6663bba7a2bb3bf7726e307f9e8b55e95d0122
mrAxekash <aliaxandr_ku@mail.ru>
2025-03-30 00:14:19 +0300

    feat: translate ban selector

--- src/widgets/users-list/ban-selector/ui/BanSelector.tsx
+++ src/widgets/users-list/ban-selector/ui/BanSelector.tsx
@@ -2,6 +2,7 @@ import { type ComponentProps } from 'react'
 
 import { UserBlockStatus } from '@/src/shared/api'
 import { Option, type OptionType, Select } from '@meetgram/ui-kit'
+import { useTranslations } from 'next-intl'
 
 const options: OptionType[] = [
   {
@@ -21,13 +22,20 @@ const options: OptionType[] = [
 type Props = {} & ComponentProps<typeof Select>
 
 export const BanSelector = ({ ...restProps }: Props) => {
+  const t = useTranslations('ban-selector')
+
   return (
-    <Select placeholder={'Not selected'} {...restProps}>
-      {options.map(o => (
-        <Option key={o.value} value={o.value}>
-          {o.label}
-        </Option>
-      ))}
+    <Select placeholder={t('Not selected')} {...restProps}>
+      {options.map(o => {
+        console.log(o)
+
+        return (
+          <Option key={o.value} value={o.value}>
+            {/*{o.label}*/}
+            {t(o.label as any)}
+          </Option>
+        )
+      })}
     </Select>
   )
 }
